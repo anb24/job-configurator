84d613242d696e5dece05927b865844192b9acae
AndreyB <AnB.124@yandex.ru>
2023-03-06 20:39:01 +0700

    basic layout
    Сверстал базовый макет

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import data from '../vendor/json_file.json' assert { type: "json" };
+console.log(data);
+
+const content = document.querySelector('.content');
+const searchInput = content.querySelector('.search__input');
+
+
+
