73d2c181bc234e7d903199fe9bfe71bb5a72f1da
AndreyB <AnB.124@yandex.ru>
2023-03-09 15:08:21 +0700

    functional extension
    работа над функционалом

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,6 @@
 //массив должностей:
 const api = 'https://raw.githubusercontent.com/aleksandrzhmak/test_task/main/json_file.json';
 
-//поиск этементов:
-const content = document.querySelector('.content');
-const searchInput = content.querySelector('.search__input');
-const searchOptions = content.querySelector('.search__options');
-const list = content.querySelector('.list');
-const addBtn = content.querySelector('.search__button');
-
 //получает и сохраняет элементы массива:
 const jobList = [];
 fetch(api)
@@ -17,8 +10,15 @@ fetch(api)
         data.forEach(function (job){
             jobList.push(job);
         });
-        // console.log(jobList);
+        // console.log('jobList >>> ', jobList);
     });
+
+//поиск этементов:
+const content = document.querySelector('.content');
+const searchInput = content.querySelector('.search__input');
+const searchOptions = content.querySelector('.search__options');
+const list = content.querySelector('.list');
+const addBtn = content.querySelector('.search__button');
 
 //фильтрует массив (совпадает ли input с данными массива):
 function getOptions(word, jobList) {
@@ -28,84 +28,70 @@ function getOptions(word, jobList) {
     })
 }
 
-//отображает отфильтрованые данные под полем ввода и передаёт в input:
+// отображает отфильтрованые данные под полем ввода и передаёт в input:
 function displayOptions() {
-    // console.log('this.value >> ', this.value);
-    const options = (this.value, jobList);
+    //console.log('this.value >>> ', this.value);
+    const options = getOptions(this.value, jobList);
+    // console.log('options >>> ', options);
     const html = options
         .map(job => {
             const regex = new RegExp(this.value, 'gi');
             const jobName = job.name.replace(regex,
                 `<span class="search__hl">${this.value}</span>`
             )
-            return `<li class="search__options_value"><span>${jobName}</span></li>`;
+            return `<li id="${job.id}"class="search__options_value"><span>${jobName}</span></li>`;
         })
-        .slice(0, 10)
+        .slice(0, 8)
         .join('');
     searchOptions.innerHTML = this.value ? html : null;
     searchOptions.classList.add('search__options_show');
 
 
 
+//с этим кодом нужно разбираться:
     const jobElement = content.querySelector('.search__options_value');
-    // console.log(jobElement);
-
-    function addElement(evt) {
-        evt.preventDefault();
-        const data = {
-            id: jobElement.id,
-            name: jobElement.name
+    
+    function addElementInInput() {
+        const jobEl = content.querySelector('.search__options_value');
+        let data = {
+            id: jobEl.id,
+            name: jobEl.textContent,
         }
-        const newCard = createCard(data);
-        list.prepend(newCard);
+        console.log('data >>> ', data);
+        const newInput = createInputVal(data);
+        return newInput
     }
-
-    jobElement.addEventListener('click', addElement);
-
-
-
-    // const jobElement = content.querySelector('.search__options_value');
-    // const cardTitle = content.querySelector('.element__title');
-    //
-    // function addElementInInput(evt) {
-    //     evt.preventDefault();
-    //     const data = {
-    //         id: jobElement.id,
-    //         name: jobElement.name
-    //     }
-    //     const input = document.querySelector('.search__input');
-    //     input.innerHTML = jobElement.value;
-    //     console.log(data.name);
-    // }
-    //
-    // function addElement(evt) {
-    //     evt.preventDefault();
-    //     const data = {
-    //         id: jobElement.id,
-    //         name: jobElement.name
-    //     }
-    //     const newCard = createCard(data);
-    //     list.prepend(newCard);
-    //     // evt.currentTarget.reset();
-    // }
-    //
-    // jobElement.addEventListener('click', addElementInInput);
-    // addBtn.addEventListener('click', addElement);
-
-
+    function createInputVal(data) {
+        console.log('data.name >>> ', data.name);
+        searchInput.value = data.name;
+        console.log('searchInputе >>> ', searchInput);
+    }
+ 
+    
+    
+    jobElement.addEventListener('click', addElementInInput);
+    
 
 }
-
-
 
 //создаёт карточку:
 function createCard(data) {
     const cardTemplate = document.querySelector('#list-template').content;
     const card = cardTemplate.querySelector('.element').cloneNode(true);
     const cardTitle = card.querySelector('.element__title');
-    cardTitle.textContent = data.name;
+    cardTitle.textContent = data;
     card.querySelector('.element__delete').addEventListener('click', deleteCard);
     return card;
+}
+
+function addElementInCard(evt) {
+    evt.preventDefault();
+    const data = searchInput.value;
+    console.log('data >>> ', data);
+    const newCard = createCard(data);
+    list.prepend(newCard);
+    searchOptions.innerHTML = null;
+    searchInput.value = '';
 }
 
 //удаляет карточку:
@@ -119,5 +105,6 @@ function deleteCard(evt) {
 }
 
 //обработчики событий:
+addBtn.addEventListener('click', addElementInCard);
 searchInput.addEventListener('change', displayOptions);
 searchInput.addEventListener('keyup', displayOptions);
